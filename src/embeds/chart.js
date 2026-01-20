fb7e473bff703eb0dc6a465ec59db9ef7db4520a
Joshua Byrd <phocks@gmail.com>
2026-01-20 15:15:25 +1000

    Will this work?

--- src/embeds/chart.js
+++ src/embeds/chart.js
@@ -2,7 +2,7 @@ import * as Plot from "npm:@observablehq/plot";
 import {FileAttachment, resize} from "observablehq:stdlib";
 
 export async function Chart() {
-  const gistemp = await FileAttachment("./lib/gistemp.csv").csv({typed: true});
+  const gistemp = await FileAttachment("../data/gistemp.csv").csv({typed: true});
   return resize((width) =>
     Plot.plot({
       width,
